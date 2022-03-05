e6982ec2d2cdb2f04e6e05073aff3d0839a073b9
zed-shasw <zed.shasw@gmail.com>
2022-03-05 11:04:34 +0530

    first prob

---
+++ DP/House Robbing/740. Delete and Earn.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/delete-and-earn/
+
+class Solution {
+public:
+    int deleteAndEarn(vector<int>& nums) {
+        int n = 10001;
+        vector<int> sum(n, 0);
+        for(int i=0; i<nums.size(); ++i)
+        {
+            sum[nums[i]] += nums[i];
+        }
+        vector<int> dp(n,0);
+        dp[1] = sum[1];
+        for(int i=2; i<n; ++i)
+        {
+            dp[i] = max(dp[i-2]+sum[i], dp[i-1]);
+        }
+        return dp[n-1];
+    }
+};
